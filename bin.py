5f87f3a289ae67b2c9772cc8c71acf693213b599
GitKoti235 <31142253+GitKoti235@users.noreply.github.com>
2018-11-25 00:22:23 +0530

    Create bin.py

---
+++ bin.py
@@ -0,0 +1,14 @@
+#Defnition
+""" It gives us at any instance the binary representation of integer"""
+
+bin(1),bin(0),bin(100000000)
+which returns 0b0 0b1 ..etc
+
+format(1,'#b') and format(1,b) and f'{1:#b}'
+#Exceptions
+Int only allowed, it clearly identify fractions as floats
+
+
+#Applications
+   1. encrypting number as binary-- Encryption
+   
