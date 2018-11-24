99c72fbd159df2fec8ec35744fe831a064999d78
GitKoti235 <31142253+GitKoti235@users.noreply.github.com>
2018-11-24 19:28:16 +0530

    abs() builtin

---
+++ abs.py
@@ -0,0 +1,19 @@
+#Defnition
+
+""" abs() takes numbers, floats, complex numbers, True, False and gives output of those absolute values, which calculates and returns """
+
+#Examples
+abs (1)
+abs(1.2)
+abs(1/2)
+abs(True)
+abs(False)
+abs(1+2j)
+
+#Special cases
+#Below throws error reg operand
+abs ("string")
+
+#APPLICAITONS
+  #when dealing with complex numbers, it ouputs 
+  #
