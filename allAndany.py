8d15361f0cc42be7c6ecd4a0e5006f2a3560a3c6
GitKoti235 <31142253+GitKoti235@users.noreply.github.com>
2018-11-24 23:26:33 +0530

    Create allAndany.py

---
+++ allAndany.py
@@ -0,0 +1,11 @@
+#DEFINITION
+""" The all() and any() they work on iterables as parameters, they used to predict whether the iterable has elements or zeros, None """
+
+#EXAMPLES
+all([2,3,4,5])
+
+#DIFFERENT_CASES
+all([0,None]) any([0,None])
+all([])
+
+""" It says that zero and None doesnt return True,also empty list its different """
